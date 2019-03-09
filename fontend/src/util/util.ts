9e575d4a2be3e03523910ee3c085e9e1a757e804
m-Ryan <962491243@qq.com>
2019-03-09 20:09:35 +0800

    fix 中文路径bug

--- fontend/src/util/util.ts
+++ fontend/src/util/util.ts
@@ -1,21 +1,23 @@
 export function comparePath(path: string, mathPath: string) {
-  const paths = path.split('/');
-  const mathPaths = mathPath.split('/');
+	path = decodeURIComponent(path);
+	mathPath = decodeURIComponent(mathPath);
+	const paths = path.split('/');
+	const mathPaths = mathPath.split('/');
 
-  if (paths.length !== mathPaths.length) {
-    if (paths.length > mathPaths.length) {
-      return false;
-    }
-    if (mathPaths[mathPaths.length - 1] !== '*') {
-      return false;
-    }
-  } else {
-    for (let i = 0; i < paths.length; i++) {
-      if (paths[i] !== mathPaths[i] && mathPaths[i] !== ':id') {
-        return false;
-      }
-    }
-  }
+	if (paths.length !== mathPaths.length) {
+		if (paths.length > mathPaths.length) {
+			return false;
+		}
+		if (mathPaths[mathPaths.length - 1] !== '*') {
+			return false;
+		}
+	} else {
+		for (let i = 0; i < paths.length; i++) {
+			if (paths[i] !== mathPaths[i] && mathPaths[i] !== ':id') {
+				return false;
+			}
+		}
+	}
 
-  return true;
+	return true;
 }
