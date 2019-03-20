f5ca1cff69d38814338d958559a78c8fbcde640d
mRyan <962491243@qq.com>
2019-03-20 09:02:37 +0800

    更新

--- fontend/build/precache-manifest.72104429ec08811c62a7b62e4c3ae41a.js
+++ fontend/build/precache-manifest.72104429ec08811c62a7b62e4c3ae41a.js
@@ -4,24 +4,24 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.5b47ec21.js"
   },
   {
-    "revision": "67556e2938cf26361c3f",
-    "url": "/static/css/main.8cda790c.chunk.css"
+    "revision": "3818cc467c2569ad5b4f",
+    "url": "/static/css/main.f53d7847.chunk.css"
   },
   {
-    "revision": "5ed2ce6d252d0b5146ea",
-    "url": "/static/js/vendors.5ed2ce6d.chunk.js"
+    "revision": "5930eb7ec50a8e1af553",
+    "url": "/static/js/vendors.5930eb7e.chunk.js"
   },
   {
     "revision": "fde004da4673562666c73cb0d3e5c9db",
     "url": "/static/media/welcome.fde004da.svg"
   },
   {
-    "revision": "06e9a967614e5e2d1131",
-    "url": "/static/js/ant-design.06e9a967.chunk.js"
+    "revision": "8d1920a9bb8b10e97416",
+    "url": "/static/js/ant-design.8d1920a9.chunk.js"
   },
   {
-    "revision": "67556e2938cf26361c3f",
-    "url": "/static/js/main.67556e29.chunk.js"
+    "revision": "3818cc467c2569ad5b4f",
+    "url": "/static/js/main.3818cc46.chunk.js"
   },
   {
     "revision": "e33471629415c521f7ab25608d016801",
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "/static/media/no_found.8c382430.png"
   },
   {
-    "revision": "06e9a967614e5e2d1131",
-    "url": "/static/css/ant-design.d49bb504.chunk.css"
+    "revision": "8d1920a9bb8b10e97416",
+    "url": "/static/css/ant-design.c9e40b7b.chunk.css"
   },
   {
-    "revision": "074662a9f1eabbbbc2882ea01accd220",
+    "revision": "0537b9be845149865b0b54037bc4a496",
     "url": "/index.html"
   }
 ];
--- fontend/build/service-worker.js
+++ fontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.800a11bc33ed893d3541e2bc5082c648.js"
+  "/precache-manifest.72104429ec08811c62a7b62e4c3ae41a.js"
 );
 
 workbox.clientsClaim();
