30e0a6c59fdcd40df29b1cdae98bd7170bb271bb
m-Ryan <962491243@qq.com>
2019-04-14 18:23:43 +0800

    编译

---
+++ fontend/build/precache-manifest.75dcfd5cbc81df9f26e448a85ae3fa06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e33471629415c521f7ab25608d016801",
+    "url": "http://public.maocanhua.cn/static/media/loginbg.e3347162.png"
+  },
+  {
+    "revision": "c05cd8f9fc38600e491a",
+    "url": "http://public.maocanhua.cn/static/css/main.4f3b977c.chunk.css"
+  },
+  {
+    "revision": "bffeb35368b071e3b036",
+    "url": "http://public.maocanhua.cn/static/js/vendors.bffeb353.chunk.js"
+  },
+  {
+    "revision": "3f2aca821da9821b1b03eb7946d78ea3",
+    "url": "http://public.maocanhua.cn/theme.js"
+  },
+  {
+    "revision": "531f628fae750cd42a8b",
+    "url": "http://public.maocanhua.cn/static/js/ant-design.531f628f.chunk.js"
+  },
+  {
+    "revision": "5056dfc2c9d659fb8a92",
+    "url": "http://public.maocanhua.cn/static/js/runtime~main.5056dfc2.js"
+  },
+  {
+    "revision": "c05cd8f9fc38600e491a",
+    "url": "http://public.maocanhua.cn/static/js/main.c05cd8f9.chunk.js"
+  },
+  {
+    "revision": "6ecf79dbb9a59248510d64773993254e",
+    "url": "http://public.maocanhua.cn/static/media/bg.6ecf79db.jpg"
+  },
+  {
+    "revision": "8c382430f673ad2237bbf19e5c8a4b00",
+    "url": "http://public.maocanhua.cn/static/media/no_found.8c382430.png"
+  },
+  {
+    "revision": "fde004da4673562666c73cb0d3e5c9db",
+    "url": "http://public.maocanhua.cn/static/media/welcome.fde004da.svg"
+  },
+  {
+    "revision": "531f628fae750cd42a8b",
+    "url": "http://public.maocanhua.cn/static/css/ant-design.5a19d1ba.chunk.css"
+  },
+  {
+    "revision": "2321157c62575c7a479c5fe9f17cdda0",
+    "url": "http://public.maocanhua.cn/index.html"
+  }
+];
