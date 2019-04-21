d215e3c758fbe05d0d7588a353e25a3af013080a
m-Ryan <962491243@qq.com>
2019-04-21 21:43:52 +0800

    fix bug

--- backend/src/upload/service/upload.service.ts
+++ backend/src/upload/service/upload.service.ts
@@ -1,7 +1,4 @@
 import { Injectable, BadRequestException, HttpService, Post } from '@nestjs/common';
-import qiniu from 'qiniu';
-import TranformToReadStream from 'tranform-to-readstream';
-import { UserError } from '../../common/filters/userError';
 import { uploadQiuNiuFile } from '../../util/upload';
 @Injectable()
 export class UploadService {
