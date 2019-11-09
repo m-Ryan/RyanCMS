140a30a68cea8820632af423d6a984cd607caeca
m-Ryan <962491243@qq.com>
2019-11-09 19:32:12 +0800

    新增上传方式

--- backend/src/upload/upload.module.ts
+++ backend/src/upload/upload.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './service/upload.service';
 import { UserController } from './controller/user.controller';
+import { VisitorController } from './controller/visitor.controller';
 
 @Module({
   imports: [],
-  controllers: [UserController],
+  controllers: [UserController, VisitorController],
   providers: [UploadService],
 })
 export class UploadModule {}
