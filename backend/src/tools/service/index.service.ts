ab438f3d6d0237b8d36b509abc4de4583c0aba8b
毛灿华 <canhua.mao@lizhiweike.com>
2019-12-03 19:25:40 +0800

    编译

--- backend/src/tools/service/index.service.ts
+++ backend/src/tools/service/index.service.ts
@@ -4,17 +4,17 @@ import { json2ts } from 'json-ts';
 import { JsonEntity } from '../controller/entities/json.entity';
 @Injectable()
 export class ToolsService {
-	constructor(private readonly httpService: HttpService) {}
+	constructor(private readonly httpService: HttpService) { }
 
 	async getJsonToTs(data: string) {
 		return json2ts(data);
 	}
 
 	async addJson(mod: string, name: string, content: string) {
-		return JsonEntity.addJson(mod, name, content)
+		return JsonEntity.addJson(mod, name, content);
 	}
 
-	async updateJson(id: number ,content: string) {
+	async updateJson(id: number, content: string) {
 		return JsonEntity.updateJson(id, content);
 	}
 
@@ -25,7 +25,7 @@ export class ToolsService {
 	async getJson(id: number) {
 		return JsonEntity.getJson(id);
 	}
-	
+
 	async getJsonList(page: number, size: number) {
 		return JsonEntity.getJsonList(page, size);
 	}
