55d6f9888068049a220f401b05c8927eb12ccf08
毛灿华 <canhua.mao@lizhiweike.com>
2019-12-04 19:08:20 +0800

    fix request entity too large

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExceptionFilter } from './common/filters/exception.filter';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ExceptionFilter());
   app.useStaticAssets(path.join(__dirname, '..', 'public'));
+  app.use(bodyParser.json({limit: '20mb'}));
+  app.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
   await app.listen(8080, () => {
     console.log('服务器已开启');
   });
